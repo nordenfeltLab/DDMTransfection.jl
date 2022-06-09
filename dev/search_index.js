36f1048e735b4a49461f85082d84100d19535b7d
Documenter.jl <documenter@juliadocs.github.io>
2022-06-09 15:38:00 +0000

    build based on 3f3f7fc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DDMTransfection","category":"page"},{"location":"#DDMTransfection","page":"Home","title":"DDMTransfection","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DDMTransfection.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DDMTransfection]","category":"page"}]
+}
